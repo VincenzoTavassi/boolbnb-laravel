ecfd2476217042b2177a15f1c77e57e3affca13f
MOHAMMED F <moa-x@hotmail.com>
2023-05-09 16:42:50 +0200

    added navbar

---
+++ resources/views/layouts/partials/searchForm.blade.php
@@ -0,0 +1,5 @@
+<!-- SEARCH BAR -->
+<div class="input-group input-group-lg m-5">
+    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+    <button class="btn btn-primary" id="inputGroup-sizing-lg">Cerca</button>
+</div>
