49fe9cfb4d1a76f075145c2377eab16cbbc934ad
Rocca-Francesco <roccafrance98@yahoo.it>
2023-05-12 10:29:34 +0200

    apartment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            User::class,
+            UserSeeder::class,
+            ApartmentSeeder::class,
         ]);
     }
 }
