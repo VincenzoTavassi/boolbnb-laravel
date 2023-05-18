fecc9c270a9582455671ec31166be4536c15c8d1
Rocca-Francesco <roccafrance98@yahoo.it>
2023-05-18 18:37:48 +0200

    message api provvisorio

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('/apartments', ApartmentController::class);
 
 Route::get('/apartments/{lat}/{lon}/{distance}', [ApartmentController::class, 'advancedSearch'])->name('advancedSearch');
+
+Route::apiResource('/messages', MessageController::class);
